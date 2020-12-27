ddad417c5dc85b24f8c65a64f9cf87be87752d84
Philippe Gouin <bering@ringlogic.com>
2020-12-27 10:39:51 -0500

    polkit and picom-git with dual_kawase blur

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -30,9 +30,10 @@ myTerminal = "alacritty"
 
 myStartupHook = do
                   spawnOnce "deadd-notification-center &"
+                  spawnOnce "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &"
                   spawnOnce "feh --no-fehbg --bg-scale '/home/phil/Images/camo tech manjaro.jpg'"
                   spawnOnce "xsetroot -cursor_name left_ptr"
-                  spawnOnce "picom &"
+                  spawnOnce "picom --experimental-backends &"
                   spawnOnce "trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --transparent true --alpha 0 --tint 0x000000 --height 18 --iconspacing 3 &"
                   spawnOnce "nm-applet &"
                   spawnOnce "pasystray &"
