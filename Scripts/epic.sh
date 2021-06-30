d3f03079ddab26ec138c58b32a22cd3d0f7c5fc6
Philippe Gouin <bering@ringlogic.com>
2021-06-30 06:50:45 -0400

    Added help message, added newly required ENV variable for launch, use -- instead of - to stop parsing arguments

--- Scripts/epic.sh
+++ Scripts/epic.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+function synthax {
+	echo "Synthax: `basename $0` <command> [app name]"
+	echo ""
+	echo "Available commands:"
+	echo -e "\tauth"
+	echo -e "\tlist-games"
+	echo -e "\tinstall <app name>"
+	echo -e "\tlaunch <app name>"
+	echo -e "\tsync-saves <app name>"
+	echo -e "\t-- <something>\t\twill call: legendary <something>"
+}
+
+if [ $# -eq 0 ]; then
+	synthax
+	exit
+fi
+
 INSTALL_DIR=`grep install_dir ~/.config/legendary/config.ini | sed -e "s/install_dir[ ]*=[ ]*//"`
 if [ -z "$INSTALL_DIR" ]; then
 	INSTALL_DIR="~/legendary"
@@ -15,6 +32,9 @@ case "$1" in
 esac
 
 case "$1" in
+	--help|-h)
+		synthax
+		;;
 	# simple commands
 	auth|list-games)
 		legendary $*
@@ -24,13 +44,13 @@ case "$1" in
 		mkdir -p "$INSTALL_DIR/Prefixes/$2" &> /dev/null
 		;;
 	launch)
-		STEAM_COMPAT_DATA_PATH="$INSTALL_DIR/Prefixes/$2" legendary launch $2 --wrapper '"/home/phil/.steam/steam/steamapps/common/Proton - Experimental/proton" run' --no-wine
+		STEAM_COMPAT_DATA_PATH="$INSTALL_DIR/Prefixes/$2" STEAM_COMPAT_CLIENT_INSTALL_PATH="$HOME/.steam/steam" legendary launch $2 --wrapper '"/home/phil/.steam/steam/steamapps/common/Proton - Experimental/proton" run' --no-wine
 		;;
 	sync-saves)
 		legendary sync-saves $2 --save-path "$INSTALL_DIR/CloudSaves/$2"
 		;;
 	# just throw everything at legendary, in case you want to do some voodoo
-	-)
+	--)
 		shift
 		legendary $*
 		;;
